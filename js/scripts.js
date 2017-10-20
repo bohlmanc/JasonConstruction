d351bd2c7dda6754e73e2490416a8db532b17c15
bohlmanc <codybohlman7@gmail.com>
2017-10-19 19:51:54 -0600

    Started working on quote page, added basic form elements, and added (non-funcitonal) js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+function SubmitQuote() {
+	var email = $("#email").val();
+	var moreInfo = $("#moreInfo").val();
+	console.log("Email: "+email);
+	console.log("Info: "+moreInfo);
+};
+
+$(document).ready();
