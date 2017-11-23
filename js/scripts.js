5ab6dc68e6de4d559d3591f6ba8618122fc4f3d0
Hailey Jones <haileyjones@Haileys-Computer.local>
2017-11-22 19:26:04 -0800

    Added services, previous work pages; navigation between pages; content on services

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@
 
 function SubmitQuote() {
 	var email = $("#email").val();
-	var moreInfo = $("#moreInfo").val();
+	var moreInfo = $("#more_info").val();
 	console.log("Email: "+email);
 	console.log("Info: "+moreInfo);
+
+	window.open('mailto:'+email+'?subject=Thank you!&body=Thank you for contacting Jones Construction!s');
+
 };
 
 $(document).ready();
